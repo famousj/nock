625d6f2d4e22cc9c64093ae69245e1aa0935849d
Justin LeBlanc <leblanc@wustl.edu>
2013-11-13 16:56:28 -0500

    Totally redid the parser, exercised all the code.  The 8 operator doesn't work, and neither does the decrement code from Chapter 2.  But everything else works and it's pretty spiffy.

--- nock.js
+++ nock.js
@@ -1,18 +1,20 @@
 #!/usr/bin/env node
 
-//N.B. Implementation functions have a `_` prefix. Public functions do not
+/*
+ * For details as to what's going on here, check out Chapter 2 of the Urbit
+ * documentation:
+ * http://www.urbit.org/2013/08/22/Chapter-2-nock.html
+ */
+
 NOCK_VERSION = "5K";
 NOCKJS_VERSION = "0.1";
 
-DEBUG = 2;
+DEBUG = 1;
 
 function showDebug(msg) {
 	if (DEBUG) console.log(msg);
 }
 
-// TODO: Add in a whole bunch of comments from Urbit docs
-
-// TODO: Starting with here, and the fact that YES is 0
 var YES = 0;
 var NO  = 1;
 
@@ -24,15 +26,9 @@ http://www.urbit.org/2013/08/22/Chapter-4-syntax.html
 function wut(noun) {
     /*
 	? :: Test whether a noun is a cell or an atom.
-
-    4  ::    ?[a b]            0
-    5  ::    ?a                1
-
-    >>> ?([1, 2])
-    0
-    >>> ?(1)
-    1
     */
+	showDebug("?" + formatList(noun));
+
 	if (Array.isArray(noun)) {
 		showDebug("4  ::    ?[a b]            0");
 		return YES;
@@ -45,84 +41,64 @@ function wut(noun) {
 
 
 function aorc(a) {
-    /* Return an atom or a properly structured cell.  
-    >>> aorc(1)
-    1
-    >>> aorc([1,2])
-    [1, 2] 
-    */
+    /* 
+	 * Return an atom or a properly structured cell.  
+	 */
     
     if (Array.isArray(a)) {
-        return properize(a);
+        return structureList(a);
     }
     else  {
 		return a;
     }
 }
 
-function properize(list) {
+function structureList(list) {
     /* Properly structure an improper list.
-
     2  ::    [a b c]           [a [b c]]
-
-    >>> properize([1])
-	ERROR
-    >>> properize([1, 2])
-    [1, 2]
-    >>> properize([1, 2, 3])
-    [1, [2, 3]]
-    >>> properize([1, 2, 3, 4])
-    [1, [2, [3, 4]]]
-    >>> properize([42, [[4, 0, 1], [3, 0, 1]]])
-    [42, [[4, [0, 1]], [3, [0, 1]]]]
     */
+	
+	// If this list isn't actually a list, return it
+	if (!Array.isArray(list))
+		return list;
 
     if (list.length == 1) {
-        var newlist = [aorc(list[0]), 0];
-		showDebug(newlist);
-		return newlist;
+		if (Array.isArray(list[0])) 
+			return [structureList(list[0]), 0];
+		else 
+			return [list[0], 0];
     }
     else if (list.length == 2) {
-        return [aorc(list[0]), aorc(list[1])];
+		return [structureList(list[0]), structureList(list[1])];
     }
     else {
-        return [aorc(list[0]), properize(list.slice(1))];
+        return [structureList(list[0]), structureList(list.slice(1))];
     }
 }
 
 function lus(noun) {
     /*
 	+ :: Increment an atom.
-
     6  ::    +[a b]            +[a b]
     7  ::    +a                1 + a
-
-    >>> lus([1, 2])
-    "lus([1, 2])"
-    >>> lus(1)
-    2
 	*/
+	showDebug("+" + formatList(noun));
 
 	if (Array.isArray(noun)) {
     	showDebug("6  ::    +[a b]            +[a b]");
-		return "+" + formatResult(noun);
+		return "+" + formatList(noun);
 	}
 	else {
-		return noun + 1;
+		showDebug("7  ::    +a                1 + a");
+		return parseInt(noun) + 1;
 	}
 }
 
 function tis(noun) {
 	/*
     = :: test for equality
-
     8  ::    =[a a]            0
     9  ::    =[a b]            1
-
-    >>> tis([1, 1])
-    0
-    >>> tis([1, 0])
-    1
     */
 	if (noun[0] == noun[1])  {
     	showDebug("8  ::    =[a a]            0");
@@ -137,23 +113,14 @@ function tis(noun) {
 function fas(list) {
 	/*
     Return the specified slot from the given noun.
-
     12 ::    /[1 a]            a
     13 ::    /[2 a b]          a
     14 ::    /[3 a b]          b
     15 ::    /[(a + a) b]      /[2 /[a b]]
     16 ::    /[(a + a + 1) b]  /[3 /[a b]]
-
-    >>> tree = [[4, 5], [6, 14, 15]]
-    >>> fas([1, tree])
-    [[4, 5], [6, [14, 15]]]
-    >>> fas((2, tree))
-    [4, 5]
-    >>> fas((3, tree))
-    [6, [14, 15]]
-    >>> fas((7, tree))
-    [14, 15]
     */
+
+	showDebug("/" + formatList(list));
 
 	var n = list[0];
 	var noun;
@@ -177,15 +144,21 @@ function fas(list) {
 	// #15, even slot index
     else if (!(n % 2)) {
 		showDebug("15 ::    /[(a + a) b]      /[2 /[a b]]");
-		showDebug(noun);
-		showDebug(formatResult(noun));
-		return "/[2 /[" + n / 2 + " " + formatResult(noun) + "]]";
+		return "/[2 /[" + n / 2 + " " + formatList(noun) + "]]";
 	}
 	// #16, odd slot index
     else {
 		showDebug("16 ::    /[(a + a + 1) b]  /[3 /[a b]]");
 		return fas([3, fas([(n - 1) / 2, noun])]);
 	}
+}
+
+function hasTwoItems(list) {
+	return (Array.isArray(list) && list.length >= 2);
+}
+
+function hasThreeItems(list) {
+	return (hasTwoItems(list) && hasTwoItems(list[1]));
 }
 
 OP_FAS = 0;
@@ -202,211 +175,277 @@ OP_H10 = 10;
 
 function tar(noun) {
     /*
-	*[a, b] -- Reduce a Nock expression.
-
-    ## 19 ::    *[a [b c] d]      [*[a b c] *[a d]]
-    >>> tar((42, ((4, 0, 1), (3, 0, 1))))
-    (43, 1)
-
-    ## 21 ::    *[a 0 b]          /[b a]
-    >>> tar((2, 0, 1))
-    2
-    >>> tar((((4, 5), (6, 14, 15)), (0, 7)))
-    (14, 15)
-
-    ## 22 ::    *[a 1 b]          b
-    >>> tar((42, 1, 5))
-    5
-    >>> tar((42, (1, 153, 218)))
-    (153, 218)
-
-    ## 23 ::    *[a 2 b c]        *[*[a b] *[a c]]
-    >>> tar((77, (2, (1, 42), (1, 1, 153, 218))))
-    (153, 218)
-
-    ## 24 ::    *[a 3 b]          ?*[a b]
-    ## 25 ::    *[a 4 b]          +*[a b]
-    ## 26 ::    *[a 5 b]          =*[a b]
-    >>> tar((57, (0, 1)))
-    57
-    >>> tar(((132, 19), (0, 3)))
-    19
-    >>> tar(((42, 43), (3, 0, 1)))
-    0
-    >>> tar((42, (3, 0, 1)))
-    1
-    >>> tar((57, (4, 0, 1)))
-    58
-    >>> tar((((57, 57), (5, 0, 1))))
-    0
-    >>> tar((((57, 58), (5, 0, 1))))
-    1
-
-    ## 28 ::    *[a 6 b c d]      *[a 2 [0 1] 2 [1 c d] [1 0] 2 [1 2 3] [1 0] 4 4 b]
-    >>> tar((42, (6, (1, 0), (4, 0, 1), (1, 233))))
-    43
-    >>> tar((42, (6, (1, 1), (4, 0, 1), (1, 233))))
-    233
-
-    ## 29 ::    *[a 7 b c]        *[a 2 b 1 c]
-    >>> tar((42, (7, (4, 0, 1), (4, 0, 1))))
-    44
-
-    ## 30 ::    *[a 8 b c]        *[a 7 [[7 [0 1] b] 0 1] c]
-    >>> tar((42, (8, (4, 0, 1), (0, 1))))
-    (43, 42)
-    >>> tar((42, (8, (4, 0, 1), (4, 0, 3))))
-    43
-
-    ## 33 ::    *[a 10 b c]       *[a c]
-    >>> tar(((132, 19), (10, 37, (4, 0, 3))))
-    20
+	* -- Reduce a Nock expression.
+    19 ::    *[a [b c] d]      [*[a b c] *[a d]]
+    21 ::    *[a 0 b]          /[b a]
+    22 ::    *[a 1 b]          b
+    23 ::    *[a 2 b c]        *[*[a b] *[a c]]
+    24 ::    *[a 3 b]          ?*[a b]
+    25 ::    *[a 4 b]          +*[a b]
+    26 ::    *[a 5 b]          =*[a b]
+    28 ::    *[a 6 b c d]      *[a 2 [0 1] 2 [1 c d] [1 0] 2 [1 2 3] [1 0] 4 4 b]
+    29 ::    *[a 7 b c]        *[a 2 b 1 c]
+    30 ::    *[a 8 b c]        *[a 7 [[7 [0 1] b] 0 1] c]
+	31 ::    *[a 9 b c]        *[a 7 c 2 [0 1] 0 b]
+	32 ::    *[a 10 [b c] d]   *[a 8 c 7 [0 3] d]
+	33 ::    *[a 10 b c]       *[a c]
     */
+	showDebug("*" + formatList(noun));
 
-    noun = properize(noun);
+	var nounString = JSON.stringify(noun);
+    noun = structureList(noun);
 
-    subj = fas([2, noun]);	// noun[0]
-    op = fas([6, noun]); 	// noun[1][0]
-    obj = fas([7, noun]);  // noun[1][1]
-	
-	// TODO: Figure out what Mr. Eyk was up to with the indent thing
+	if (!hasThreeItems(noun))
+			throw Error("Invalid parameters for tar: " + nounString);
+
+    var a = noun[0];
+	var op = noun[1][0];
+    var obj = noun[1][1];
 	
 	// #19
-	if (wut(op) == YES) {
-		console.log("<- 19 ::    *[a [b c] d]      [*[a b c] *[a d]]");
-		return (tar([subj, op]), tar([subj, obj]));
+	if (Array.isArray(op)) {
+		showDebug("19 ::    *[a [b c] d]      [*[a b c] *[a d]]");
+		return [tar([a, op]), tar([a, obj])];
 	}
 	// #21: tree addressing
 	else if (op == OP_FAS) {
-		console.log("<- 21 ::    *[a 0 b]          /[b a]");
-		return fas([obj, subj]);
+    	showDebug("21 ::    *[a 0 b]          /[b a]");
+		return fas([obj, a]);
 	}
 	// #22: constant operator
 	else if (op == OP_CON) {
-		console.log("<- 22 ::    *[a 1 b]          b");
-		return obj
+		showDebug("22 ::    *[a 1 b]          b");
+		return obj;
 	}
 	// #23: recursion
 	else if (op == OP_TAR) { 
-		console.log("<- 23 ::    *[a 2 b c]        *[*[a b] *[a c]]");
-		b = fas([2, obj]);
-		c = fas([3, obj]);
-		return tar([tar([subj, b]), tar([subj, c])]);
+		showDebug("23 ::    *[a 2 b c]        *[*[a b] *[a c]]");
+
+		if (!hasTwoItems(obj))
+			throw Error("Invalid arguments for the 2 operator");
+
+		b = obj[0];
+		c = obj[1];
+		return tar([tar([a, b]), tar([a, c])]);
 	}
 	// #24: ?
 	else if (op == OP_WUT) { 
-		console.log("<- 24 ::    *[a 3 b]          ?*[a b]");
-		return wut(tar([subj, obj]));
+		showDebug("24 ::    *[a 3 b]          ?*[a b]");
+		return wut(tar([a, obj]));
 	}
 	// #25: +
 	else if (op == OP_LUS) { 
-		console.log("<- 25 ::    *[a 4 b]          +*[a b]");
-		return lus(tar([subj, obj]));
+		showDebug("25 ::    *[a 4 b]          +*[a b]");
+		return lus(tar([a, obj]));
 	}
 	// #26: =
 	else if (op == OP_TIS) { 
 		console.log("<- 26 ::    *[a 5 b]          =*[a b]");
-		return tis(tar([subj, obj]));
+		return tis(tar([a, obj]));
 	}
 	// #28: if
 	else if (op == OP_IF) { 
-		console.log("<- 28 ::    *[a 6 b c d]      *[a 2 [0 1] 2 [1 c d] [1 0] 2 [1 2 3] [1 0] 4 4 b]");
-		a = subj
-		b = fas([2, obj]);
-		c = fas([6, obj]);
-		d = fas([7, obj]);
+		console.log("28 ::    *[a 6 b c d]      *[a 2 [0 1] 2 [1 c d] [1 0] 2 [1 2 3] [1 0] 4 4 b]");
+		if (!hasThreeItems(obj))
+			throw Error("Invalid arguments for the 6 operator");
+
+		b = obj[0];
+		c = obj[1][0];
+		d = obj[1][1];
 		return tar([a, 2, [0, 1], 2, [1, c, d], [1, 0], 2, [1, 2, 3], [1, 0], 4, 4, b]); 
+		// The reduced version:
+		//return tar([a, tar([[c, d], [0, tar([[2, 3], 
+		//			[0, lus(lus(tar([a, b])))]])]])]);
 	}
+	// #29: Function composition
 	else if (op == OP_H07) {
-		console.log("<- 29 ::    *[a 7 b c]        *[a 2 b 1 c]");
-		b = fas([2, obj]);
-		c = fas([3, obj]);
-		return tar([subj, 2, b, 1, c]); 
+		showDebug("29 ::    *[a 7 b c]        *[a 2 b 1 c]");
+		if (!hasTwoItems(obj))
+			throw Error("Invalid arguments for the 7 operator");
+
+		b = obj[0];
+		c = obj[1];
+		return tar([a, 2, b, 1, c]); 
+		// The reduced version:
+		//return  tar([tar([a, b]), c]);
 	}
+	// #30: function composition with ordered pair
 	else if (op == OP_H08) {
-		console.log("<- 30 ::    *[a 8 b c]        *[a 7 [[7 [0 1] b] 0 1] c]");
-		b = fas([2, obj]);
-		c = fas([3, obj]); 
-		return tar([subj, 7, [[7, [0, 1], b], 0, 1], c]); 
+		showDebug("30 ::    *[a 8 b c]        *[a 7 [[7 [0 1] b] 0 1] c]");
+		if (!hasTwoItems(obj))
+			throw Error("Invalid arguments for the 8 operator");
+
+		b = obj[0];
+		c = obj[1];
+
+		// TODO: This uses the reduction from Chap 2's appendix.  The unreduced
+		// version doesn't quite work as expected.
+		//return tar([a, 7, [[7, [0, 1], b], 0, 1], c]); 
+		return tar([[tar([a, b]),  a], c]);
 	}
+	// #31: core
 	else if (op == OP_H09) {
-		console.log("<- 31 ::    *[a 9 b c]        *[a 7 c 2 [0 1] 0 b]");
-		b = fas([2, obj]);
-		c = fas([3, obj]);
-		return tar([subj, 7, c, 2, [0, 1], 0, b]); 
+		showDebug("31 ::    *[a 9 b c]        *[a 7 c 2 [0 1] 0 b]");
+		if (!hasTwoItems(obj))
+			throw Error("Invalid arguments for the 9 operator");
+
+		b = obj[0];
+		c = obj[1];
+
+		// TODO: Don't have any kind of test code for this.  The decrement
+		// routine in chapter 2 makes use operation 9, and that totally doesn't
+		// work at all.
+
+		return tar([a, 7, c, 2, [0, 1], 0, b]); 
 	}
 	else if (op == OP_H10) {
-		hint = fas([2, obj]);
-		if (wut(hint) == YES) {
-			console.log("<- 32 ::    *[a 10 [b c] d]   *[a 8 c 7 [0 3] d]");
-			c = fas([2, hint]);
-			return tar([subj, 8, c, 7, [0, 3], obj]);
+		if (!hasTwoItems(obj)) 
+				throw Error("Invalid arguments for the 10 operator");
+
+		hint = obj[0];
+		if (Array.isArray(hint)) {
+			if (!hasTwoItems(obj[0])) 
+				throw Error("Invalid arguments for the 10 operator");
+				
+			showDebug("32 ::    *[a 10 [b c] d]   *[a 8 c 7 [0 3] d]");
+			c = obj[0][1];
+			d = obj[1];
+
+			// TODO: No test for this either.  See above about decrement,
+			// though.
+			return tar([a, 8, c, 7, [0, 3], d]);
+			// The reduced version:
+			//return tar([a d])
 		}
 		else {
-			console.log("<- 33 ::    *[a 10 b c]       *[a c]");
-			c = fas([3, obj]);
-			return tar([subj, c]);
+			showDebug("33 ::    *[a 10 b c]       *[a c]");
+			return tar([subj, obj[1]]);
 		}
 	}
+}
+
+var operators = "[\\?\\+\\=\\/\\*]";
+
+function tokenize(str) {
+	/* 
+	 * Returns an array of tokens for a given nock expression
+	 */
+
+	var original = str;
+	var tokens = new Array;
+
+	while (str != "") {
+		var operators_regex = new RegExp("^(" + operators + ")\s*(.*)");
+		// If it's an operator
+		if ((match = str.match(operators_regex)) != null) {
+			tokens.push(match[1]);
+			str = match[2];
+		}
+		// If it's an atom
+		else if ((match = str.match(/^(\d+)\s*(.*)/)) != null) {
+			tokens.push(match[1]);
+			str = match[2];
+		}
+		// If it's either sort of bracket
+		else if ((match = str.match(/^\s*([\[\]])\s*(.*)/)) != null) {
+			tokens.push(match[1]);
+			str = match[2];
+		}
+		else {
+			throw Error("Invalid input: \"" + original + "\"");
+		}
+	}
+
+	return tokens;
 }
 
 function parseNock(str) {
 	/*
-	 * Take a nock string and generate the equivalent JavaScript command
+	 * Take a nock string and generate the equivalent JavaScript function
 	 */
 	if (DEBUG > 1) console.log("Parsing: '" + str + "'");
-	if (str == "") {
-		return "";
+
+	// This will only work if we start with one of our operators
+
+	if (!str.match(operators)) {
+		console.log("Invalid function: " + str);
+		console.log("Functions should start with one of the following: ? / * + =");
 	}
 
-	if (str[0] == '?') {
-		return "wut(" + parseNock(str.slice(1)) + ")";
-	}
-	else if (str[0] == "/") {
-		return "fas(" + parseNock(str.slice(1)) + ")";
-	}
-	else if (str[0] == "+") {
-		return "lus(" + parseNock(str.slice(1)) + ")";
-	}
-	else if (str[0] == "*") {
-		return "tar(" + parseNock(str.slice(1)) + ")";
-	}
-	else if (str[0] == "=") {
-		return "tis(" + parseNock(str.slice(1)) + ")";
-	}
+	var tokens = tokenize(str);
 
-	// Remove matching sel and ser
-	if ((matches = str.match(/^(.*)\[([^\[\]]+)\](.*)$/)) != null) {
-		return parseNock(matches[1]) + 
-			   "[" + parseNock(matches[2]) + "]" +
-			   parseNock(matches[3]);
+	return readFromTokens(tokens);
+}
+
+function readFromTokens(tokens) {
+	/*
+	 * Take the token array and generate a function
+	 */
+	
+	var token = tokens.shift();
+	token = token + "";
+
+	// If it's an operator, return the appropriate operator function, and 
+	// recursively call this function to get the parameters
+	if ((match = token.match(operators)) != null) {
+		var params = readFromTokens(tokens);
+		if (token == "?") {
+			return function() {
+				return wut(params);
+			}
+		}
+		else if (token == "+") {
+			return function() {
+				return lus(params);
+			}
+		}
+		else if (token == "=") {
+			return function() {
+				return tis(params);
+			}
+		}
+		else if (token == "/") {
+			return function() {
+				return fas(params);
+			}
+		}
+		else if (token == "*") {
+			return function() {
+				return tar(params);
+			}
+		}
 	}
-
-	// if the next character is 
-
-	// digits followed by possible whitespace, and end of line 
-	if ((matches = str.match(/^(\d+\s*)$/)) != null) 
-		return matches[1];
-
-	// digits with a space and then something else get a comma :0
-	if ((matches = str.match(/^(\d+)\s+(.+)/)) != null) 
-		return matches[1] + ", " + parseNock(matches[2]);
-
-	// If we're still here, we got something weird
-	throw Error("Unexpected input: " + str);
+	// Accept all unsigned integers
+	else if (token.match(/\d+/)) {
+		return token;
+	}
+	if (token == "[") {
+		var array = new Array();
+		while (tokens[0] != "]")  {
+			array.push(readFromTokens(tokens));
+		}
+		tokens.shift();
+		return array;
+	}
+	else if (token == "]") {
+		throw Error("Unmatched ]");
+	}
+	else {
+		
+		// If we're still here, we got something weird
+		throw Error("Unexpected input: " + token);
+	}
 }
 
 
-function formatResult(result) {
+function formatList(result) {
 	/*
-	 * Take the javascript return value and format it to look nocky
+	 * Take the JavaScript return value and format it to look like nock
 	 */
 
-	// The return value should be either an atom or an array.
-	// The array could be an array of arrays.  
 
-	if (DEBUG > 1) console.log("formatting " + result);
-
+	// The return value should be either an atom or an array.  Or a string in a
+	// crash condition.  The array could be an array of arrays.  
 	if (!Array.isArray(result))
 		return result + "";
 
@@ -417,7 +456,7 @@ function formatResult(result) {
 		}
 
 		if (Array.isArray(result[i]))
-			returnVal += formatResult(result[i]);
+			returnVal += formatList(result[i]);
 		else 
 			returnVal += result[i]
 	}		
@@ -427,40 +466,15 @@ function formatResult(result) {
 }
 
 function nock(command) {
-	while (typeof command == "string") {
-		if (DEBUG > 1) console.log(command);
+	var jsFunction = parseNock(command);
 
-		// TODO: Yes, I'm using the dreaded eval statement.  This is version
-		// 0.1 and this is  the least amount of code to write.   Since the 
-		// parser immediately blows up on anything not an operator, digit, 
-		// [ or ], I don't // _think_ this would be an issue.  But I'm not 
-		// sure.  Hence TODO here is to think up a cuter way of doing things.
-		var jsCommand = parseNock(command);
-
-
-		var result = eval(jsCommand);
-
-		// Don't loop infinitely.  
-		// TODO: think of some other kind of infinite loop detection
-		if (formatResult(result) == command) {
-			console.log(result);
-			showDebug("Exiting now to avoid an infinite loop");
-			return;
-		}
-		
-		command = result;
-
-		if (typeof(command) == "string")
-			showDebug(command);
-		else 
-			console.log(formatResult(result));
-	}
+	var result = jsFunction();
+	console.log(formatList(result));
 }
 
 'use strict';
 
-console.log("Nock ver. " + NOCK_VERSION);
-console.log("Nock.js ver. " + NOCKJS_VERSION);
+console.log("Nock ver. " + NOCK_VERSION + "; Nock.js ver. " + NOCKJS_VERSION);
 console.log("Control-C to exit");
 
 process.stdout.write("> ");
@@ -471,7 +485,8 @@ process.stdin.on('data', function(line) {
 	line = line + "";
 	line = line.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 
-	nock(line);
+	if (line != "") 
+		nock(line);
 	
 	process.stdout.write("> ");
 });
